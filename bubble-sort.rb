971e6230fb07a6a1e459e777d2234a1052243c5e
Jake <jakegreen2016@gmail.com>
2020-11-02 13:25:14 -0900

    wrote pseudo-code for the method

---
+++ bubble-sort.rb
@@ -0,0 +1,15 @@
+#declare variable for array
+
+#define method for bubble-sort
+
+    #cycle through the array until array is sorted in ascending order
+
+        #for each pass through the array
+            
+            #test if each element value is greater than the element to its right.
+
+                #if greater, swap the values
+
+                #if not greater, ignore
+
+    #print the array after it is completely sorted
