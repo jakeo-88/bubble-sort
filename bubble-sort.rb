96bf002ceb23d666ac343a6fc8363020b52a52bc
Jake <jakegreen2016@gmail.com>
2020-11-02 14:13:06 -0900

    create a loop that compares each element value to the element on its right, if the left element is higher in value, the elements swap places

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,13 +3,21 @@
 #define method for bubble-sort
 
     #cycle through the array until array is sorted in ascending order
-
-        #for each pass through the array
             
-            #test if each element value is greater than the element to its right.
+        #for each pass through the arraytest if each element value is greater than the element to its right.
+        i = 0
+        while i < (array_to_sort.length - 1) do
+            #if greater, swap the values
+            if array_to_sort[i] > array_to_sort[i+1]
+                c = array_to_sort[i]
+                array_to_sort[i] = array_to_sort[i+1]
+                array_to_sort[i+1] = c
 
-                #if greater, swap the values
-
-                #if not greater, ignore
-
+            #if not greater, ignore
+            else
+            
+            end
+            i += 1
+        end
+            
     #print the array after it is completely sorted
