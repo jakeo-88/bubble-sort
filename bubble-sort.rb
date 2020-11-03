2f4bc9ab86ba1934bccd6e0cca9d4c638d0e61f1
Jake <jakegreen2016@gmail.com>
2020-11-03 10:37:38 -0900

    add another loop which runs the single-pass-loop the maximum number of times necessary to completely rearrange an array

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,7 +3,8 @@
 #define method for bubble-sort
 
     #cycle through the array until array is sorted in ascending order
-            
+    j = 0
+    while j < (array_to_sort.length - 1) do  
         #for each pass through the arraytest if each element value is greater than the element to its right.
         i = 0
         while i < (array_to_sort.length - 1) do
@@ -19,5 +20,6 @@
             end
             i += 1
         end
-            
+        j += 1
+    end
     #print the array after it is completely sorted
